c062b7c11b73753d474421a31808120b5bf5c1af
ckm960411 <a01098956117@gmail.com>
2024-03-30 14:30:14 +0900

    TypeOrmModule AppModule에 추가

--- cf_sns/src/app.module.ts
+++ cf_sns/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      /** 데이터베이스 타입 */
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // 개발환경에서만 true
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
